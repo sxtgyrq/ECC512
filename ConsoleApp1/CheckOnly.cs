bed200c67ae64d537f0317efec51b14a59ab31c2
瑞卿 要 <sxtgyrq@163.com>
2020-02-02 23:26:16 +0800

    20200202

--- ConsoleApp1/CheckOnly.cs
+++ ConsoleApp1/CheckOnly.cs
@@ -12,25 +12,27 @@ namespace ConsoleApp1
         public static void Check()
         {
             Console.WriteLine("输入文件夹，如F:\\生活\\ysecret");
-            DirectoryInfo root = new DirectoryInfo(Console.ReadLine());
-            FileInfo[] files = root.GetFiles();
+
+            var files = getAll(Console.ReadLine());
+            //DirectoryInfo root = new DirectoryInfo();
+            //FileInfo[] files = root.GetFiles();
 
             Dictionary<string, string> md5s = new Dictionary<string, string>();
-            for (int indexOfFileForCompressing = 0; indexOfFileForCompressing < files.Length; indexOfFileForCompressing++)
+            for (int indexOfFileForCompressing = 0; indexOfFileForCompressing < files.Count; indexOfFileForCompressing++)
             {
                 //bool isRight;
-                Console.WriteLine($"{files[indexOfFileForCompressing].FullName}");
-                var md5 = GetMD5HashFromFile(files[indexOfFileForCompressing].FullName);
+                Console.WriteLine($"{files[indexOfFileForCompressing]}");
+                var md5 = GetMD5HashFromFile(files[indexOfFileForCompressing]);
                 if (md5s.ContainsKey(md5))
                 {
                     Console.WriteLine("检验到文件冲突");
                     Console.WriteLine(md5s[md5]);
-                    Console.WriteLine(files[indexOfFileForCompressing].FullName);
+                    Console.WriteLine(files[indexOfFileForCompressing]);
                     return;
                 }
                 else
                 {
-                    md5s.Add(md5, files[indexOfFileForCompressing].FullName);
+                    md5s.Add(md5, files[indexOfFileForCompressing]);
                 }
             }
         }
@@ -57,6 +59,39 @@ namespace ConsoleApp1
             {
                 throw new Exception("GetMD5HashFromFile() fail,error:" + ex.Message);
             }
+        }
+
+
+        public static void Check2()
+        {
+            Console.WriteLine("输入文件夹，如F:\\生活\\ysecret");
+            var path = Console.ReadLine();
+            getAll(path);
+        }
+        static List<string> getAll(string path)
+        {
+            List<string> result = new List<string>();
+
+            DirectoryInfo root = new DirectoryInfo(path);
+            FileInfo[] files = root.GetFiles();
+            for (int indexOfFileForCompressing = 0; indexOfFileForCompressing < files.Length; indexOfFileForCompressing++)
+            {
+                //bool isRight;
+                //  Console.WriteLine($"{files[indexOfFileForCompressing].FullName}");
+                result.Add(files[indexOfFileForCompressing].FullName);
+            }
+            var directories = root.GetDirectories();
+
+            for (var i = 0; i < directories.Length; i++)
+            {
+                var m = getAll(directories[i].FullName);
+                for (var j = 0; j < m.Count; j++)
+                {
+                    result.Add(m[j]);
+                }
+            }
+
+            return result;
         }
     }
 }
